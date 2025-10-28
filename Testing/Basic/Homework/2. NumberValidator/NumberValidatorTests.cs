4d87d606fc14d79eb1498b66dbdd8c03b22a56a5
Vladimir Golubev <gor.prast@bk.ru>
2025-10-29 01:49:23 +0500

    test: add NumberValidator ctor tests

--- Testing/Basic/Homework/2. NumberValidator/NumberValidatorTests.cs
+++ Testing/Basic/Homework/2. NumberValidator/NumberValidatorTests.cs
@@ -1,31 +1,30 @@
-﻿
+﻿using FluentAssertions;
 using NUnit.Framework;
-using NUnit.Framework.Legacy;
 
 namespace HomeExercise.Tasks.NumberValidator;
 
 [TestFixture]
 public class NumberValidatorTests
 {
-    [Test]
-    public void Test()
+    [TestCase(1, 0, TestName = "PositivePrecision_ScaleIsZero")]
+    [TestCase(2, 1, TestName = "PositivePrecisionAndScale_ScaleLessThanPrecision")]
+    public void Ctor_WithValidParameters_ShouldNotThrow(int precision, int scale)
     {
-        Assert.Throws<ArgumentException>(() => new NumberValidator(-1, 2, true));
-        Assert.DoesNotThrow(() => new NumberValidator(1, 0, true));
-        Assert.Throws<ArgumentException>(() => new NumberValidator(-1, 2, false));
-        Assert.DoesNotThrow(() => new NumberValidator(1, 0, true));
+        var act = () => new NumberValidator(precision, scale);
 
-        ClassicAssert.IsTrue(new NumberValidator(17, 2, true).IsValidNumber("0.0"));
-        ClassicAssert.IsTrue(new NumberValidator(17, 2, true).IsValidNumber("0"));
-        ClassicAssert.IsTrue(new NumberValidator(17, 2, true).IsValidNumber("0.0"));
-        ClassicAssert.IsFalse(new NumberValidator(3, 2, true).IsValidNumber("00.00"));
-        ClassicAssert.IsFalse(new NumberValidator(3, 2, true).IsValidNumber("-0.00"));
-        ClassicAssert.IsTrue(new NumberValidator(17, 2, true).IsValidNumber("0.0"));
-        ClassicAssert.IsFalse(new NumberValidator(3, 2, true).IsValidNumber("+0.00"));
-        ClassicAssert.IsTrue(new NumberValidator(4, 2, true).IsValidNumber("+1.23"));
-        ClassicAssert.IsFalse(new NumberValidator(3, 2, true).IsValidNumber("+1.23"));
-        ClassicAssert.IsFalse(new NumberValidator(17, 2, true).IsValidNumber("0.000"));
-        ClassicAssert.IsFalse(new NumberValidator(3, 2, true).IsValidNumber("-1.23"));
-        ClassicAssert.IsFalse(new NumberValidator(3, 2, true).IsValidNumber("a.sd"));
+        act.Should().NotThrow();
     }
+    
+    [TestCase(-1, 2, TestName = "NegativePrecision")]
+    [TestCase(0, 1, TestName = "ZeroPrecision")]
+    [TestCase(1, -1, TestName = "NegativeScale")]
+    [TestCase(3, 4, TestName = "ScaleGreaterThanPrecision")]
+    [TestCase(3, 3, TestName = "ScaleEqualsPrecision")]
+    public void Ctor_WithInvalidParameters_ShouldThrow(int precision, int scale)    
+    {
+        var act = () => new NumberValidator(precision, scale);
+
+        act.Should().Throw<ArgumentException>();
+    }
+
 }
