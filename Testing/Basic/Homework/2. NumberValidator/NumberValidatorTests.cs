774007121c5a80cab75ec69900902029528d0881
Vladimir Golubev <gor.prast@bk.ru>
2025-10-30 12:17:28 +0500

    refactor: add message check in NumberValidatorTests

--- Testing/Basic/Homework/2. NumberValidator/NumberValidatorTests.cs
+++ Testing/Basic/Homework/2. NumberValidator/NumberValidatorTests.cs
@@ -6,6 +6,9 @@ namespace HomeExercise.Tasks.NumberValidator;
 [TestFixture]
 public class NumberValidatorTests
 {
+    private const string precisionExceptionMessage = "precision must be a positive number";
+    private const string scaleExceptionMessage = "scale must be a non-negative number less or equal than precision";
+    
     [TestCase(1, 0, TestName = "PositivePrecision_ScaleIsZero")]
     [TestCase(2, 1, TestName = "PositivePrecisionAndScale_ScaleLessThanPrecision")]
     public void Ctor_WithValidParameters_ShouldNotThrow(int precision, int scale)
@@ -15,16 +18,17 @@ public class NumberValidatorTests
         act.Should().NotThrow();
     }
 
-    [TestCase(-1, 2, TestName = "NegativePrecision")]
-    [TestCase(0, 1, TestName = "ZeroPrecision")]
-    [TestCase(1, -1, TestName = "NegativeScale")]
-    [TestCase(3, 4, TestName = "ScaleGreaterThanPrecision")]
-    [TestCase(3, 3, TestName = "ScaleEqualsPrecision")]
-    public void Ctor_WithInvalidParameters_ShouldThrow(int precision, int scale)
+    [TestCase(-1, 2, precisionExceptionMessage, TestName = "NegativePrecision")]
+    [TestCase(0, 1,precisionExceptionMessage, TestName = "ZeroPrecision")]
+    [TestCase(1, -1,scaleExceptionMessage, TestName = "NegativeScale")]
+    [TestCase(3, 4, scaleExceptionMessage, TestName = "ScaleGreaterThanPrecision")]
+    [TestCase(3, 3, scaleExceptionMessage, TestName = "ScaleEqualsPrecision")]
+    public void Ctor_WithInvalidParameters_ShouldThrow(int precision, int scale, string message)
     {
         var act = () => new NumberValidator(precision, scale);
 
-        act.Should().Throw<ArgumentException>();
+        act.Should().Throw<ArgumentException>()
+            .WithMessage(message);
     }
 
     [TestCase("12.34", TestName = "DotSeparator")]
